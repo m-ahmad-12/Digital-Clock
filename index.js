506c5696042c489b9f94fa37ffaccaa8e98bccd4
Muhammad Ahmad <ahmedshahid216513@gmail.com>
2026-05-24 13:05:00 +0500

    Add padding

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let clock=document.querySelector(".digital_clock");
 let liveTimer=function(){
    let currentDate=new Date();
-   clock.textContent=`${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`};
+let hours = String(currentDate.getHours());
+let minutes=String(currentDate.getMinutes());
+let seconds=String(currentDate.getSeconds());
+hours=hours.padStart(2,"0");
+minutes=minutes.padStart(2,"0");
+ clock.textContent=`${hours}:${minutes}:${seconds}`};
 
 setInterval(liveTimer,1000)
 
