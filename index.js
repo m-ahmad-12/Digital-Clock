ac0bca9987b24b4d91b436862d36c61e387444ab
Muhammad Ahmad <ahmedshahid216513@gmail.com>
2026-05-24 19:40:06 +0500

    added a countdown timer

--- index.js
+++ index.js
@@ -14,7 +14,6 @@
 
       let stopWatch=document.querySelector(".stop_watch");
 
-
       let start=document.querySelector(".start");
       let stops=document.querySelector(".stop");
       let reset=document.querySelector(".reset");
@@ -33,10 +32,8 @@
       stopWatch.textContent=`${hours}:${minutes}:${seconds}`;
       }
       start.onclick=function(){
-
+    if(interval !== null) return;
       interval=setInterval(main,1000);
-
-
       };
 
    stops.onclick=function(){
@@ -47,6 +44,43 @@
    totalSeconds = 0
    stopWatch.textContent = "00:00:00"
    }  
+let display=document.querySelector(".countdown");
+let five=document.querySelector(".five");
+let ten=document.querySelector(".ten");
+let fifteen=document.querySelector(".fifteen");
+let countdownbtn=document.querySelector(".countdownbtn");
+five.onclick=function(){
+sec+=5;
+};
+ten.onclick=function(){
+sec+=10;
+};
+fifteen.onclick=function(){
+sec+=15;
+};
+
+let sec=0;
+let intervals=null;
+function count(){  
+let mins=Math.floor(sec/60);
+let secs=(sec%60);
+let hours=Math.floor(mins/60);
+   hours = String(hours).padStart(2, "0")
+   mins = String(mins).padStart(2, "0")
+   secs = String(secs).padStart(2, "0")
+  display.textContent=`${hours}:${mins}:${secs}`;
+  sec=sec-1;
+if(sec == 0){
+    clearInterval(intervals);
+    intervals = null;
+    display.textContent = "00:00:00";
+}
+}
+countdownbtn.onclick=function(){
+if(intervals!==null){return};
+intervals=setInterval(count,1000);
+
+};
 
 
 
